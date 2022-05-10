d5243e67100a86befedbc29a008fc1932c861ea2
VanHau87 <nguyenvanh29@gmail.com>
2022-05-10 21:24:38 +0700

    Configuration routers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Following from "./pages/Following";
-import Home from "./pages/Home";
+import { publicRoutes } from "~/routes";
+import { DefaultLayout } from "~/components/Layout";
+import { Fragment } from "react";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/following" element={<Following />} />
+          {publicRoutes.map((route, idx) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            if (route.layout) Layout = route.layout;
+            if (route.layout === null) Layout = Fragment;
+            return (
+              <Route
+                key={idx}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "~/components/Layout/Header";
+import Sidebar from "./Sidebar";
+
+function DefaultLayout({ children }) {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Sidebar />
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
